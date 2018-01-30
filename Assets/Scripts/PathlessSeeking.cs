dec9c0086af1158782bfaadb35dbbc97ef0ae8ce
Shea McAuley <s179049@students.aie.edu.au>
2018-01-29 17:33:39 -0800

    Added grid system

---
+++ Assets/Scripts/PathlessSeeking.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathlessSeeking : MonoBehaviour
+{
+
+    Rigidbody rb;
+   
+    Vector3 desiredVelocity;
+    Vector3 projectedPosition;
+    public float projectedDistance;
+    public float speed;
+    public float BurstSpeed;
+    float currentSpeed;
+    public Transform target;
+    Rigidbody targetRb;
+
+    float dist;
+    public float acceptableDistance;
+
+
+    void Start()
+    {
+        currentSpeed = speed;
+        rb = GetComponent<Rigidbody>();
+        targetRb = target.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, target.position);
+
+        if (dist > acceptableDistance)
+        {
+            currentSpeed = speed;
+            projectedPosition = target.position + (targetRb.velocity.normalized * projectedDistance);
+            desiredVelocity = -speed * (target.transform.position - transform.position).normalized;
+
+            rb.AddForce(desiredVelocity - rb.velocity);
+        }
+        else
+        {
+            speed = BurstSpeed;
+            desiredVelocity = speed * (target.position - transform.position).normalized;
+            rb.AddForce(desiredVelocity - rb.velocity);
+        }
+
+        Debug.DrawLine(transform.position, projectedPosition, Color.red);
+        Debug.DrawLine(transform.position, target.position, Color.blue);
+        Debug.DrawLine(transform.position, desiredVelocity * 10, Color.green);
+    }
+}
---
+++ Assets/Scripts/WallAvoidBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallAvoidBehavior : MonoBehaviour {
+
+    Rigidbody rb;
+
+    public float avoidanceStrength;
+    public float avoidanceRange;
+
+    Vector3 wallDir;
+	// Use this for initialization
+	void Start () {
+        wallDir = transform.forward;
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+    public void doWallAvoidance()
+    {
+        RaycastHit hit;
+       
+        if (Physics.Raycast(transform.position, wallDir, out hit, avoidanceRange))
+        {
+            rb.AddForce(hit.normal * avoidanceStrength);
+        }
+        else
+        {
+            Vector3 wallDir = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        doWallAvoidance();
+	}
+}
