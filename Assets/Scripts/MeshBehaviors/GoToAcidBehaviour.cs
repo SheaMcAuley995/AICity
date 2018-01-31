d9ef2cb0043f7b28df3489348b8367754589829d
Shea McAuley <s179049@students.aie.edu.au>
2018-01-31 12:11:42 -0800

    Learned stacks

---
+++ Assets/Scripts/MeshBehaviors/GoToAcidBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToAcidBehaviour : Task {
+
+    public GameObject acid;
+
+    public override void doBehaviour(BehaviorManager manger)
+    {
+        manger.agent.destination = acid.transform.position;
+    }
+
+    public override bool checkBehavior(BehaviorManager manager)
+    {
+        if(manager.health < 25)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override void updateBehavior(BehaviorManager manager)
+    {
+        if(checkBehavior(manager))
+        {
+
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
